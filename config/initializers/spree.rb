e76c4aabf2cabea376924da0e289fa3d108360d9
alex <akhamutov@ibs.ru>
2011-07-04 16:45:25 +0400

    fixes #496

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -2,4 +2,5 @@ if Spree::Config.instance
   Spree::Config.set(:allow_ssl_in_production => false)
   Spree::Config.set(:default_locale => 'ru') 
   Spree::Config.set(:address_requires_state => false)
+  Spree::Config.set(:products_per_page => 12)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110614105203) do
+ActiveRecord::Schema.define(:version => 20110314192118) do
 
   create_table "addresses", :force => true do |t|
     t.string   "firstname"
@@ -382,11 +382,9 @@ ActiveRecord::Schema.define(:version => 20110614105203) do
 
   create_table "properties", :force => true do |t|
     t.string   "name"
-    t.string   "presentation",                     :null => false
+    t.string   "presentation", :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "use_in_filter", :default => false, :null => false
-    t.string   "filter_type"
   end
 
   create_table "properties_prototypes", :id => false, :force => true do |t|
