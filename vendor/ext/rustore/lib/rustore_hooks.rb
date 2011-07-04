71d9c845f5ee09ab33c7b44472902e3ff237adb5
andrew <andrew@.(none)>
2011-07-04 17:27:36 +0400

    fixes #498. sku and sizes on products

--- vendor/ext/rustore/lib/rustore_hooks.rb
+++ vendor/ext/rustore/lib/rustore_hooks.rb
@@ -1,3 +1,25 @@
 class RustoreHooks < Spree::ThemeSupport::HookListener
-  # custom hooks go here
+  insert_before :product_properties do
+    %(
+      <dl class="table-display">
+      <% if @product.master.sku? %>
+      <dt><%= t("sku") %></dt>
+      <dd><%= @product.master.sku %></dd>
+      <% end %>
+    <% unless @product.has_variants? %>
+      <% if @product.master.width? %>
+      <dt><%= t("width") %></dt>
+      <dd><%= @product.master.width %></dd>
+      <% end %>
+      <% if @product.master.height? %>
+      <dt><%= t("height") %></dt>
+      <dd><%= @product.master.height %></dd>
+      <% end %>
+      <% if @product.master.depth? %>
+      <dt><%= t("depth") %></dt>
+      <dd><%= @product.master.depth %></dd>
+      <% end %>
+     <% end %>
+    </dl>)
+  end
 end
