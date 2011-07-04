6ca9acddc502a81952c4850986194b21a8e245d5
andrew <andrew@.(none)>
2011-07-04 17:39:27 +0400

    small refactoring

--- vendor/ext/rustore/lib/rustore_hooks.rb
+++ vendor/ext/rustore/lib/rustore_hooks.rb
@@ -1,27 +1,5 @@
 class RustoreHooks < Spree::ThemeSupport::HookListener
-  insert_before :product_properties do
-    %(
-      <dl class="table-display">
-      <% if @product.master.sku? %>
-      <dt><%= t("sku") %></dt>
-      <dd><%= @product.master.sku %></dd>
-      <% end %>
-    <% unless @product.has_variants? %>
-      <% if @product.master.width? %>
-      <dt><%= t("width") %></dt>
-      <dd><%= @product.master.width %></dd>
-      <% end %>
-      <% if @product.master.height? %>
-      <dt><%= t("height") %></dt>
-      <dd><%= @product.master.height %></dd>
-      <% end %>
-      <% if @product.master.depth? %>
-      <dt><%= t("depth") %></dt>
-      <dd><%= @product.master.depth %></dd>
-      <% end %>
-     <% end %>
-    </dl>)
-  end
+  insert_before :product_properties , 'shared/attributes'
 
   replace :cart_item_description, 'order/order_item_detail'
 end
