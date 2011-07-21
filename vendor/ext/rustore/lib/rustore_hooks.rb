bd6ca8920734ddc3a4ed624e7ba450f1433a6048
andrew <andrew@.(none)>
2011-07-21 17:13:22 +0400

    has permitted to update shipment always

--- vendor/ext/rustore/lib/rustore_hooks.rb
+++ vendor/ext/rustore/lib/rustore_hooks.rb
@@ -5,4 +5,5 @@ class RustoreHooks < Spree::ThemeSupport::HookListener
   replace :product_description, 'products/description'
   replace :cart_item_description, 'order/order_item_detail'
   replace :admin_order_show_buttons, 'admin/orders/order_buttons'
+  replace :admin_shipment_edit_form_buttons, 'admin/shipments/shipment_buttons'
 end
